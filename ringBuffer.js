57502f9f0839fb547c999b707bba12b59700dc72
Emma Drueke <emma.drueke@gmaila.com>
2018-08-21 16:55:52 -0500

    created append and get methods

--- ringBuffer.js
+++ ringBuffer.js
@@ -4,3 +4,13 @@ class Buffer {
     this.storage = [],
     this.current = 0   
   }
+  append(item) {
+    this.storage[this.current++] = item
+    if (this.current === this.capacity) {
+      this.current = 0
+    }
+  }
+  get(item) {
+    return this.storage
+  }
+}
